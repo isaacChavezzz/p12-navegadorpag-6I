0e2518aae8cfb7fcf1cca5ddff1cd2a1ebdb6fc8
isaacChavezzz <143744084+isaacChavezzz@users.noreply.github.com>
2024-03-01 13:28:30 -0700

    Update pantalla1.dart

--- lib/pantalla1.dart
+++ lib/pantalla1.dart
@@ -7,17 +7,45 @@ class Pantalla1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Pantalla1"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-            child: const Text("Ir a Pantalla2"),
-            onPressed: () {
-              Navigator.pushNamed(context, "/pantalla2",
-                  arguments: "Mensaje desde la pantalla1");
-            }),
-      ),
-    );
+        backgroundColor: Color(0xff024d5a),
+        appBar: AppBar(
+          title: const Text("Pantalla1 isaac chavez"),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Text("Isaac Abel Chavez Nolasco"),
+            Icon(
+              Icons.headphones,
+              size: 109,
+              color: Colors.orange,
+            ),
+            ElevatedButton(
+                child: const Text("Ir a Pantalla2"),
+                onPressed: () {
+                  Navigator.pushNamed(context, "/pantalla2",
+                      arguments: "Mensaje desde la pantalla1");
+                }),
+            Card(
+              child: Container(
+                color: Colors.amber,
+                width: double.infinity,
+                height: 300,
+                child: Card(
+                  color: Colors.black,
+                  margin: EdgeInsets.all(32),
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Text(
+                      'Pantalla 1',
+                      style: TextStyle(fontSize: 20, color: Colors.amber),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
