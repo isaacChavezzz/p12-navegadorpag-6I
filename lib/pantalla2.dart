53540a3c105abdfe73d54bdceb16a7028c43d20e
isaacChavezzz <143744084+isaacChavezzz@users.noreply.github.com>
2024-03-01 13:28:15 -0700

    Update pantalla2.dart

--- lib/pantalla2.dart
+++ lib/pantalla2.dart
@@ -8,18 +8,44 @@ class Pantalla2 extends StatelessWidget {
     final argumento = ModalRoute.of(context)!.settings.arguments as String;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Pantalla2"),
-      ),
-      body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text(argumento),
-        ElevatedButton(
-            child: const Text("Regresar a Pantalla1"),
-            onPressed: () {
-              Navigator.pop(context);
-            }),
-      ])),
-    );
+        backgroundColor: Color(0xff75b6ea),
+        appBar: AppBar(
+          title: const Text("Pantalla2 isaac chavez"),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Text("isaac Abel Chavez Nolasco"),
+            Icon(
+              Icons.headphones,
+              size: 109,
+              color: Colors.deepOrange,
+            ),
+            ElevatedButton(
+                child: const Text("Regresar a Pantalla1"),
+                onPressed: () {
+                  Navigator.pop(context);
+                }),
+            Card(
+              child: Container(
+                color: Colors.amber,
+                width: double.infinity,
+                height: 300,
+                child: Card(
+                  color: Colors.black,
+                  margin: EdgeInsets.all(32),
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Text(
+                      'Pantalla 2',
+                      style: TextStyle(fontSize: 30, color: Colors.amber),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
